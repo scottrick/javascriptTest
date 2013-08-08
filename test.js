e3b4dd5bd3d9aab107d81f4a82aecaee0316c28b
scottrick <scottrick49@gmail.com>
2013-08-07 20:02:32 -0500

    Lots of work on the board

--- test.js
+++ test.js
@@ -29,6 +29,95 @@ console.log(canvas.style);
 
 var context = canvas.getContext("2d");
 
+var BOARD_SPACE_SIZE = 32;
+
+var BOARD_SPACE_EMPTY = 0;
+var BOARD_SPACE_WORM = 1;
+var BOARD_SPACE_APPLE = 2;
+
+var Board = { };
+Board.width =  Math.floor(canvas.width / BOARD_SPACE_SIZE);
+Board.height = Math.floor(canvas.height / BOARD_SPACE_SIZE);
+Board.spaces = new Array();
+
+Board.setSpace = function(x, y, newValue) {
+	Board.spaces[x + y * Board.width] = newValue;
+}
+
+Board.getSpace = function(x, y) {
+	return Board.spaces[x + y * Board.width];
+}
+
+Board.initialize = function() {
+	for (var x = 0; x < Board.width; x++) {
+		for (var y = 0; y < Board.height; y++) {
+			Board.setSpace(x, y, BOARD_SPACE_EMPTY);
+		}
+	}
+
+	Board.placeApple();
+
+	// Board.setSpace(3, 5, BOARD_SPACE_APPLE);
+	// Board.setSpace(0, 0, BOARD_SPACE_WORM);
+	// Board.setSpace(1, 2, BOARD_SPACE_WORM);
+	// Board.setSpace(13, 15, BOARD_SPACE_WORM);
+	// Board.setSpace(13, 16, BOARD_SPACE_WORM);
+	// Board.setSpace(13, 17, BOARD_SPACE_WORM);
+}
+
+Board.placeApple = function() {
+	//calculate how many empty spaces we have
+	var numberOfEmptySpaces = 0;
+
+	for (var x = 0; x < Board.width; x++) {
+		for (var y = 0; y < Board.height; y++) {
+			if (Board.getSpace(x, y) == BOARD_SPACE_EMPTY) {
+				numberOfEmptySpaces++;
+			}
+		}
+	}
+
+	//choose a random empty space
+	var space = Math.floor(Math.random() * numberOfEmptySpaces);
+	
+	var currentSpace = 0;
+	for (var x = 0; x < Board.width; x++) {
+		for (var y = 0; y < Board.height; y++) {
+			if (Board.getSpace(x, y) == BOARD_SPACE_EMPTY) {
+				if (currentSpace == space) {
+					//found our space!!!!!
+					Board.setSpace(x, y, BOARD_SPACE_APPLE);
+					return;
+				}
+				else {
+					currentSpace++;
+				}
+			}
+		}
+	}
+}
+
+Board.dump = function() {
+	for (var y = 0; y < Board.height; y++) {
+		var line = "" + y;
+
+		while (line.length < 4) {
+			line += " ";
+		}
+
+		line += " > ";
+
+		for (var x = 0; x < Board.width; x++) {
+			line += "" + Board.getSpace(x, y);
+		}
+
+		console.log(line);
+	}
+}
+
+Board.initialize();
+// Board.dump();
+
 var Worm = { };
 Worm.velocity = 1250;
 Worm.size = 24;
@@ -63,7 +152,7 @@ Game.run = (function() {
 				Game.update();
 				nextGameTick += skipTicks;
 				loops++;
-			}
+			}		
 			
 			if (!loops) {
 				Game.draw((nextGameTick - (new Date).getTime()) / skipTicks);
@@ -79,10 +168,34 @@ Game.draw = function() {
 	context.fillStyle = "#000"
 	context.fillRect(0, 0, canvas.width, canvas.height);
 	
-	context.fillStyle = "#4F4"
-	
+	var xInset = Math.floor((canvas.width - BOARD_SPACE_SIZE * Board.width) / 2);
+	var yInset = Math.floor((canvas.height - BOARD_SPACE_SIZE * Board.height) / 2);
+
+	context.fillStyle = "#444";
+
 	context.beginPath();
-	context.fillRect(Worm.positionX - Worm.size / 2, Worm.positionY - Worm.size / 2, Worm.size, Worm.size);
+	context.fillRect(0, 0, canvas.width, yInset);
+	context.fillRect(canvas.width - xInset, 0, xInset, canvas.height);
+	context.fillRect(0, 0, xInset, canvas.height);
+	context.fillRect(0, canvas.height - yInset, canvas.width, yInset);
+
+	for (var x = 0; x < Board.width; x++) {
+		for (var y = 0; y < Board.height; y++) {
+			var spaceContents = Board.getSpace(x, y);
+
+			if (spaceContents > BOARD_SPACE_EMPTY) {
+				//space has something in it!  So lets draw it...
+				if (spaceContents == BOARD_SPACE_APPLE) {
+					context.fillStyle = "#B00";
+					context.fillRect(xInset + x * BOARD_SPACE_SIZE, yInset + y * BOARD_SPACE_SIZE, BOARD_SPACE_SIZE, BOARD_SPACE_SIZE);
+				}
+				else if (spaceContents == BOARD_SPACE_WORM) {
+					context.fillStyle = "#0B0";
+					context.fillRect(xInset + x * BOARD_SPACE_SIZE, yInset + y * BOARD_SPACE_SIZE, BOARD_SPACE_SIZE, BOARD_SPACE_SIZE);
+				}
+			}
+		}
+	}
 };
 
 Game.update = function() { 
